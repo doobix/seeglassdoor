2d4220a64e4db20b5213059a089e67295a441854
seewes <doobix@gmail.com>
2014-11-19 14:29:09 -0800

    Get tab url and search it on Glassdoor

--- popup.js
+++ popup.js
@@ -1,25 +1,46 @@
-var QUERY = 'glassdoor';
-
+// My secret Glassdoor keys
 var glassdoor_id = '27251';
 var glassdoor_Key = 'e6BJypHHlMY';
 
+// Company to search for
+var QUERY = '';
+
+// Get current tab's URL
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+    QUERY = getDomainName(url);
+
+    // Glassdoor API
+    var search = 'http://api.glassdoor.com/api/api.htm?t.p=' + glassdoor_id + 
+                 '&t.k=' + glassdoor_Key + '&q=' + QUERY + 
+                 '&userip=0.0.0.0&useragent=&format=json&v=1&action=employers';
+
+    // Get Glassdoor results
+    if (QUERY) {
+      $.get(search, searcher);
+    }
+});
+
+// Extract a domain name from a string
+var getDomainName = function(string) {
+  var regex = new RegExp("\/\/(.+)\/");
+  var domainName = regex.exec(string);
+console.log('searching',string);
+console.log('found',domainName);
+  if (domainName[1]) {
+    return domainName[1];
+  } else {
+    return null;
+  }
+}
+
+// Display Glassdoor results
 var searcher = function(data) {
   console.log(data);
 
-  if (data.success) {
+  if (data.success && data.response.totalRecordCount > 0) {
     $("#seeglassdoor").append('<h2>' + data.response.employers[0].name + '</h2>');
   } else {
     $("#seeglassdoor").append('<h2>No results found on Glassdoor</h2>');
   }
 }
-
-var search = 'http://api.glassdoor.com/api/api.htm?t.p=' + glassdoor_id + 
-             '&t.k=' + glassdoor_Key + 
-             '&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q=' + 
-             QUERY + '&callback=searcher';
-var searchScript = '<script type="text/javascript" src="' + search + '"></script>'
-
-// Run our kitten generation script as soon as the document's DOM is ready.
-document.addEventListener('DOMContentLoaded', function () {
-  $('#seeglassdoor').append(searchScript);
-});
