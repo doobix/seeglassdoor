cbd6cf3d8ae90180a40d6a61a8903b3bd3144b65
seewes <doobix@gmail.com>
2014-11-24 13:56:08 -0800

    Add star ratings

--- js/popup.js
+++ js/popup.js
@@ -35,23 +35,77 @@ chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
 //   }
 // }
 
+// Create the 5 star visual display
+var generateStars = function(rating, size) {
+  size = size || '';
+
+  if (size === 'mini') {
+    size = '-mini';
+  }
+
+  // Classes for the stars
+  var starBG = 'star-bg' + size;
+  var starGreen = 'star-green' + size;
+  var star = 'star' + size;
+
+  var result = '';
+  var decimal = rating;
+
+  // Create full green stars
+  for (var decimal = rating; decimal >= 1; decimal--) {
+    result += '<i class="' + starBG + '"><i class="' + starGreen + '"><i class="' + star + '"></i></i></i>';
+  }
+
+  // Create fractional green star
+  if (decimal > 0) {
+    var percent = decimal * 100 + '%';
+    result += '<i class="' + starBG + '"><i class="' + starGreen + '" style="width: ' +
+              percent + '"><i class="' + star + '"></i></i></i>';
+  }
+
+  // Create gray stars
+  var grayStars = Math.floor(5 - rating);
+  if (grayStars >= 1) {
+    for (var i = 0; i < grayStars; i++) {
+      result += '<i class="' + starBG + '"><i class="' + star + '"></i></i>';
+    }
+  }
+
+  return result;
+}
+
 // Display Glassdoor results
 var searcher = function(data) {
   console.log(data);
   var result = data.response.employers[0];
 
   if (data.success && data.response.totalRecordCount > 0) {
-    $("#logo").append('<img src="' + result.squareLogo + '" />');
-    $("#name").append(result.name);
-    $("#reviews").append(result.numberOfRatings + ' reviews');
-    $("#overallRating .rating").append(result.overallRating);
-    $("#cultureAndValuesRating .rating").append(result.cultureAndValuesRating);
-    $("#workLifeBalanceRating .rating").append(result.workLifeBalanceRating);
-    $("#seniorLeadershipRating .rating").append(result.seniorLeadershipRating);
-    $("#compensationAndBenefitsRating .rating").append(result.compensationAndBenefitsRating);
-    $("#careerOpportunitiesRating .rating").append(result.careerOpportunitiesRating);
-    $("#moreInfo").append('<a target="_blank" href="http://www.glassdoor.com/api/api.htm?version=1&action=employer-overview&t.s=w-m&t.a=c&employerId=' + result.id + '">View Reviews</a>');
+    $('#logo').append('<img src="' + result.squareLogo + '" />');
+    $('#name').append(result.name);
+    $('#reviews').append('<a target="_blank" href="http://www.glassdoor.com/api/api.htm?version=1&action=employer-overview&t.s=w-m&t.a=c&employerId=' + 
+      result.id + '">' + result.numberOfRatings + ' reviews</a>');
+
+    $('#overallRating .rating').append(result.overallRating);
+    $('#overallRating .stars').append(generateStars(result.overallRating));
+
+    $('#cultureAndValuesRating .rating').append(result.cultureAndValuesRating);
+    $('#cultureAndValuesRating .stars').append(generateStars(result.cultureAndValuesRating, 'mini'));
+
+    $('#workLifeBalanceRating .rating').append(result.workLifeBalanceRating);
+    $('#workLifeBalanceRating .stars').append(generateStars(result.workLifeBalanceRating, 'mini'));
+
+    $('#seniorLeadershipRating .rating').append(result.seniorLeadershipRating);
+    $('#seniorLeadershipRating .stars').append(generateStars(result.seniorLeadershipRating, 'mini'));
+
+    $('#compensationAndBenefitsRating .rating').append(result.compensationAndBenefitsRating);
+    $('#compensationAndBenefitsRating .stars').append(generateStars(result.compensationAndBenefitsRating, 'mini'));
+
+    $('#careerOpportunitiesRating .rating').append(result.careerOpportunitiesRating);
+    $('#careerOpportunitiesRating .stars').append(generateStars(result.careerOpportunitiesRating, 'mini'));
+
+    // $('#moreInfo').append('<a target="_blank" href="http://www.glassdoor.com/api/api.htm?version=1&action=employer-overview&t.s=w-m&t.a=c&employerId=' + result.id + '">View Reviews</a>');
   } else {
-    $("#seeglassdoor").append('<h2>No results found on Glassdoor</h2>');
+    $('#seeglassdoor').empty();
+    $('#seeglassdoor').append('<h2>No results found on Glassdoor</h2>');
   }
 }
